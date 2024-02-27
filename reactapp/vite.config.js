974939a0c06dc6e3950dd62a1e42bc23bc1bb5b8
goldmember80 <tankemaster@gmail.com>
2024-02-27 22:39:44 +1300

    Vite base modified

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -24,7 +24,7 @@ const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    //base:"Payslip",
+    base:"Payslip",
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
