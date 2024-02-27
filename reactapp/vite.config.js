e349ae0989f54b8e6bc414dda7b2f73db6adb631
goldmember80 <tankemaster@gmail.com>
2024-02-27 19:27:44 +1300

    Test base of vite build

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -24,6 +24,7 @@ const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base:"Payslip",
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
